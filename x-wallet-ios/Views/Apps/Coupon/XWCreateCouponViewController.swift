5c36d77adfed0fd543ed5aea1ea07aea6df7b41a
linkio <linkio.hu@gmail.com>
2018-03-27 13:16:30 +0800

    add coupon

---
+++ x-wallet-ios/Views/Apps/Coupon/XWCreateCouponViewController.swift
@@ -0,0 +1,38 @@
+//
+//  XWCreateCouponViewController.swift
+//  x-wallet-ios
+//
+//  Created by 胡波 on 2018/3/27.
+//  Copyright © 2018年 linkio. All rights reserved.
+//
+
+import UIKit
+
+class XWCreateCouponViewController: UIBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveAction(_ : UIButton) {
+        let vc = self.navigationController?.viewControllers[1] as! XWCouponListViewController
+        self.navigationController?.popToViewController(vc, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
