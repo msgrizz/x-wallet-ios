5b215edcb767668dda7f76078d60229ffcdb41d1
Evrins <evrins.hu@gmail.com>
2018-04-11 10:48:37 +0800

    new main UI

---
+++ x-wallet-ios/Views/Main/CardDocument/XWCardBaseView.swift
@@ -0,0 +1,28 @@
+//
+//  XWCardBaseView.swift
+//  x-wallet-ios
+//
+//  Created by 爱班 on 2018/4/11.
+//  Copyright © 2018年 evrins. All rights reserved.
+//
+
+import UIKit
+
+class XWCardBaseView: UIView {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var newMessageTag: UIImageView!
+    @IBOutlet weak var starTag: UIButton!
+
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ x-wallet-ios/Views/Main/CardDocument/XWCardHeadView.swift
@@ -0,0 +1,23 @@
+//
+//  XWCardHeadView.swift
+//  x-wallet-ios
+//
+//  Created by 爱班 on 2018/4/11.
+//  Copyright © 2018年 evrins. All rights reserved.
+//
+
+import UIKit
+
+class XWCardHeadView: UIView {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
